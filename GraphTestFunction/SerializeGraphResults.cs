0e35d915cdbe68a04ef3aaf30bb1b9f50a938153
Chris House <chhouse@microsoft.com>
2024-06-19 12:50:27 -0500

    simplified response serialization

--- GraphTestFunction/SerializeGraphResults.cs
+++ GraphTestFunction/SerializeGraphResults.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
@@ -16,55 +14,34 @@ public class SerializeGraphResults
     {
         var serializedStream = new MemoryStream();
         var isValid = false;
-        var headers = ConvertHeadersToDictionary(response.Headers);
-        var headersJson = JObject.Parse(JsonConvert.SerializeObject(headers));
+
+        var builder = new StringBuilder();
         var responseJson = await response.Content.ReadAsStringAsync();
-        byte[] jsonBytes = null;
+
+        foreach (var header in response.Headers)
+        {
+            builder.Append(header.Key).Append(": ").AppendJoin(", ", header.Value).Append(Environment.NewLine);
+        }
+
+        builder.AppendLine();
+        builder.AppendLine(responseJson);
+        var outputBytes = Encoding.UTF8.GetBytes(builder.ToString());
 
         try
         {
-            var bodyJson = JObject.Parse(responseJson);
-            var output = new JObject
-            {
-                ["headers"] = headersJson,
-                ["body"] = bodyJson
-            };
+            JObject.Parse(responseJson);
 
-            var bodyAndHeaders = output.ToString();
-
-            jsonBytes = Encoding.UTF8.GetBytes(bodyAndHeaders);
             isValid = true;
         }
         catch (JsonReaderException e)
         {
-            jsonBytes = Encoding.UTF8.GetBytes(CreateOutputForInvalidJson(responseJson, headersJson.ToString()));
             Console.WriteLine(e);
         }
-
-        await serializedStream.WriteAsync(jsonBytes, 0, jsonBytes.Length);
+        
+        await serializedStream.WriteAsync(outputBytes, 0, outputBytes.Length);
         await serializedStream.FlushAsync();
         serializedStream.Position = 0;
         
         return (serializedStream, isValid);
-    }
-
-    static string CreateOutputForInvalidJson(string body, string headers) => $"{headers}{Environment.NewLine}{body}";
-
-    static Dictionary<string, string> ConvertHeadersToDictionary(HttpResponseHeaders httpResponseHeaders)
-    {
-        var headers = new Dictionary<string, string>();
-
-        foreach (var header in httpResponseHeaders)
-        {
-            if (header.Value != null)
-            {
-                var value = string.Join(" ", header.Value)
-                    .TrimEnd(" ".ToCharArray());
-
-                headers.Add(header.Key, value);
-            }   
-        }
-
-        return headers;
     }
 }
