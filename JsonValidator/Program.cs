877c3010bee7240fd21871d5cfa55a2cf59fddc9
Christopher House <chhouse@microsoft.com>
2024-07-08 17:17:23 -0500

    implementation paused since this probably isn't needed (#7)

---
+++ JsonValidator/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json;
+
+namespace JsonValidator;
+
+using System.CommandLine;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var inputDirOption = new Option<DirectoryInfo>(name: "--input-dir", description: "The directory containing the JSON files to validate.")
+        {
+            IsRequired = true
+        };
+
+        var outputFileOption = new Option<FileInfo?>(name: "--output-file", description: "The file to write the validation results to.  Output will be written to the console even if this option isn't set.")
+        {
+            IsRequired = false
+        };
+
+        var rootCmd = new RootCommand("Validate JSON files");
+        rootCmd.AddOption(inputDirOption);
+        rootCmd.AddOption(outputFileOption);
+
+        rootCmd.SetHandler<DirectoryInfo, FileInfo>(ValidateJsonFiles, inputDirOption, outputFileOption);
+        Console.ReadLine();
+    }
+
+    private static void ValidateJsonFiles(DirectoryInfo inputDir, FileInfo? outputFile)
+    {
+        var jsonFiles = inputDir.EnumerateFiles("*.json", SearchOption.AllDirectories);
+
+        foreach (var file in jsonFiles)
+        {
+            
+        }
+    }   
+}
